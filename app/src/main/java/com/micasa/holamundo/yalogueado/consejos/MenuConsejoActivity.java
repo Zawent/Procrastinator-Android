34ac53d3a0da5cbf07136f343d4ed195120a9c77
Zawent <fabianvargas1099@gmail.com>
2024-02-20 09:13:24 -0500

    Listar consejos, consejo diario, y cosas mas

---
+++ app/src/main/java/com/micasa/holamundo/yalogueado/consejos/MenuConsejoActivity.java
@@ -0,0 +1,67 @@
+package com.micasa.holamundo.yalogueado.consejos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.micasa.holamundo.DataInfo;
+import com.micasa.holamundo.R;
+import com.micasa.holamundo.adapter.ConsejoAdapter;
+import com.micasa.holamundo.model.Consejo;
+import com.micasa.holamundo.network.ConsejoAPICliente;
+import com.micasa.holamundo.network.ConsejoAPIService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class MenuConsejoActivity extends AppCompatActivity {
+
+    TextView numeroNivel;
+    ListView lista;
+    private ConsejoAPIService serviceC;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_consejo);
+        numeroNivel = findViewById(R.id.numNivel);
+        numeroNivel.setText(""+DataInfo.respuestaLogin.getUser().getNivel_id());
+        lista = findViewById(R.id.listaConsejo);
+        serviceC = ConsejoAPICliente.getConsejoService();
+    }
+
+    protected void onStart(){
+        super.onStart();
+        loadData();
+    }
+
+    private void loadData(){
+        serviceC.getAll(DataInfo.respuestaLogin.getToken_type()+" "+DataInfo.respuestaLogin.getAccess_token(), DataInfo.respuestaLogin.getUser().getNivel_id()).enqueue(new Callback<List<Consejo>>() {
+            @Override
+            public void onResponse(Call<List<Consejo>> call, Response<List<Consejo>> response) {
+                if (response.isSuccessful()) {
+                    List<Consejo> consejos = response.body();
+                    cargarConsejos(consejos);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Consejo>> call, Throwable t) {
+                Log.e("error", String.valueOf(t));
+            }
+        });
+    }
+
+    private void cargarConsejos(List<Consejo> consejos) {
+        ConsejoAdapter datos = new ConsejoAdapter(consejos, this);
+        lista.setAdapter(datos);
+    }
+
+}
