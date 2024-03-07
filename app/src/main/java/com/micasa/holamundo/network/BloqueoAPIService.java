da0eecfc1ccdb5b0e5b093c538d47c5a4ee43982
Heidy13 <h.andreaortiz1325@gmail.com>
2024-03-06 21:14:38 -0500

    literalmente solo dos lineas de codigo, termino en el sena, en el BloqueoAPIService

--- app/src/main/java/com/micasa/holamundo/network/BloqueoAPIService.java
+++ app/src/main/java/com/micasa/holamundo/network/BloqueoAPIService.java
@@ -4,10 +4,12 @@ import com.micasa.holamundo.model.Bloqueo;
 
 import java.sql.Time;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
@@ -18,6 +20,7 @@ public interface BloqueoAPIService {
     @POST("bloqueo")
     Call<Bloqueo> crearBloqueo(@Header("Authorization") String authorization, @Field("hora_inicio") Time hora_inicio, @Field("duracion")Time duracion, @Field("estado") String estado, @Field("id_app") long id_app);
 
-
+    @GET("bloqueados/topApps/{id_user}")
+    Call<Bloqueo> topApps(@Header("Authorization")String authorization);
 
 }
