8f9e7d48c5e53ff94eb8db28e275f2e491258e19
Anthony <128050080+Planettte@users.noreply.github.com>
2024-04-05 08:06:47 -0500

    Delete java/com/micasa/holamundo directory

--- java/com/micasa/holamundo/DataInfo.java
+++
@@ -1,8 +0,0 @@
-package com.micasa.holamundo;
-
-import com.micasa.holamundo.model.RespuestaLogin;
-
-public class DataInfo {
-//Aqui es donde esta la informacion del usuario.
-    public static RespuestaLogin respuestaLogin = null;
-}
